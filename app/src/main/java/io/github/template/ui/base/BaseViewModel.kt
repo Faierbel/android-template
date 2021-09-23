1695ef0a6761723324d1c27d9bcfed1b3d5a3b1b
Rafał Borcz <RafalBO99@outlook.com>
2021-09-23 14:25:01 +0200

    Add navigateTo

--- app/src/main/java/io/github/template/ui/base/BaseViewModel.kt
+++ app/src/main/java/io/github/template/ui/base/BaseViewModel.kt
@@ -3,9 +3,15 @@ package io.github.template.ui.base
 import androidx.lifecycle.ViewModel
 import androidx.navigation.NavDirections
 import io.github.template.util.LiveEvent
+import io.github.template.util.toLiveData
 
 abstract class BaseViewModel : ViewModel() {
 
-    val navCommand = LiveEvent<NavDirections>()
+    private val _navCommand = LiveEvent<NavDirections>()
 
+    val navCommand get() = _navCommand.toLiveData()
+
+    protected fun navigateTo(directions: NavDirections) {
+        _navCommand.value = directions
+    }
 }
